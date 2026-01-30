0d221d9ea4e94ccd88c8b00ce38ae148bdca1785
AKtomik <97125853+AKtomik@users.noreply.github.com>
2026-01-30 03:06:55 +0100

    hi command

---
+++ src/main/java/io/github/aktomik/voicechatdictator/command/HiCommand.java
@@ -0,0 +1,54 @@
+package io.github.aktomik.voicechatdictator.command;
+
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import io.github.aktomik.voicechatdictator.brigadier.BrigadierCommand;
+import io.github.aktomik.voicechatdictator.brigadier.BrigadierToolbox;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import io.papermc.paper.command.brigadier.Commands;
+import io.papermc.paper.command.brigadier.argument.ArgumentTypes;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+
+public class HiCommand extends BrigadierCommand {
+
+	@Override
+	public String name() {
+		return "hi";
+	}
+
+	@Override
+	public String permission() {
+		return null;
+	}
+
+	@Override
+	public String description() {
+		return "say hi";
+	}
+
+	@Override
+	public List<String> aliases() {
+		return List.of();
+	}
+
+	@Override
+	public LiteralArgumentBuilder<CommandSourceStack> root() {
+		return base()
+		.then(Commands.argument("players", ArgumentTypes.players())
+		.executes(exe));
+	}
+
+	Command<CommandSourceStack> exe = (ctx) -> {
+		final List<Player> players = BrigadierToolbox.resolvePlayers(ctx);
+
+		for (Player player : players)
+		{
+			player.sendRichMessage("<yellow>hi <you>!", Placeholder.parsed("you", player.getName()));
+		}
+
+		return Command.SINGLE_SUCCESS;
+	};
+}
