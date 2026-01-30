485b4e39d4b4a8c8bcbe38eb8a057d74ead0097b
AKtomik <97125853+AKtomik@users.noreply.github.com>
2026-01-30 03:46:29 +0100

    register dictatein

--- src/main/java/io/github/aktomik/voicechatdictator/VoicechatDictator.java
+++ src/main/java/io/github/aktomik/voicechatdictator/VoicechatDictator.java
@@ -1,6 +1,7 @@
 package io.github.aktomik.voicechatdictator;
 
 import io.github.aktomik.voicechatdictator.brigadier.BrigadierToolbox;
+import io.github.aktomik.voicechatdictator.command.DictateinCommand;
 import io.github.aktomik.voicechatdictator.command.DictateoutCommand;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -20,7 +21,7 @@ public final class VoicechatDictator extends JavaPlugin {
     @Override
     public void onEnable() {
         voicechatPlugin = new VoicechatCop(this);
-        BrigadierToolbox.loadCommands(this, List.of( new DictateoutCommand(this, voicechatPlugin)));
+        BrigadierToolbox.loadCommands(this, List.of(new DictateoutCommand(this, voicechatPlugin), new DictateinCommand(this, voicechatPlugin)));
     }
 
     @Override
